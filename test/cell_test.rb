eab21c25653c442fc305a8f525ed5ea240628b43
Hanna Davis <hanna.e.davis@gmail.com>
2020-08-25 18:05:45 -0600

    Add fired upon? method

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -34,4 +34,12 @@ class CellTest < Minitest::Test
     refute cell.empty?
   end
 
+  def test_it_is_not_fired_upon_initially
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell.place_ship(cruiser)
+
+    refute cell.fired_upon?
+  end
 end
