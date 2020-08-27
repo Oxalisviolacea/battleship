0b57c89a510df435ad413cc6b407b406fc3a04f0
Eduardo Parra <helloeduardo@icloud.com>
2020-08-27 15:32:02 -0600

    Add Game class and test it has attributes

---
+++ lib/game.rb
@@ -0,0 +1,9 @@
+class Game
+  attr_reader :player_board, :computer_board
+  
+  def initialize
+    @player_board = Board.new
+    @computer_board = Board.new
+  end
+
+end
--- test/game_test.rb
+++ test/game_test.rb
@@ -1,3 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/turn'
+require './lib/game'
+
+class GameTest < Minitest::Test
+
+  def test_it_exists
+    game = Game.new
+
+    assert_instance_of Game, game
+  end
+
+  def test_it_has_a_two_boards
+    game = Game.new
+
+    assert_instance_of Board, game.player_board
+    assert_instance_of Board, game.computer_board
+  end
+
+end
+
 =begin
 - start
  call every method
