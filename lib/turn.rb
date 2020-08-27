33ed6aceebc8ed9b10e4cbe5055d65656371c888
Eduardo Parra <helloeduardo@icloud.com>
2020-08-26 20:51:04 -0600

    Add display board

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,16 @@
 class Turn
+  attr_reader :player_board, :computer_board
 
-  def initialize
+  def initialize(player_board, computer_board)
+    @player_board = player_board
+    @computer_board = computer_board
+  end
 
+  def display_boards
+    "=============COMPUTER BOARD=============\n" +
+    computer_board.render +
+    "==============PLAYER BOARD==============\n" +
+    player_board.render(true)
   end
 
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -8,9 +8,44 @@ require './lib/turn'
 class TurnTest < Minitest::Test
 
   def test_it_exists
-    turn = Turn.new
+    player_board = Board.new
+    computer_board = Board.new
+
+    turn = Turn.new(player_board, computer_board)
 
     assert_instance_of Turn, turn
+  end
+
+  def test_it_has_a_two_boards
+    player_board = Board.new
+    computer_board = Board.new
+
+    turn = Turn.new(player_board, computer_board)
+
+    assert_instance_of Board, turn.player_board
+    assert_instance_of Board, turn.computer_board
+  end
+
+  def test_it_can_display_both_boards
+    player_board = Board.new
+    computer_board = Board.new
+
+    turn = Turn.new(player_board, computer_board)
+
+    boards = "=============COMPUTER BOARD=============\n" +
+             "  1 2 3 4 \n" +
+              "A . . . . \n" +
+              "B . . . . \n" +
+              "C . . . . \n" +
+              "D . . . . \n" +
+              "==============PLAYER BOARD==============\n" +
+              "  1 2 3 4 \n" +
+              "A . . . . \n" +
+              "B . . . . \n" +
+              "C . . . . \n" +
+              "D . . . . \n"
+
+    assert_equal boards, turn.display_boards
   end
 
 end
