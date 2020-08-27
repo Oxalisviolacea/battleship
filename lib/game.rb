a978ade618282db92cf5a3169e5a3c7073a8e740
Eduardo Parra <helloeduardo@icloud.com>
2020-08-27 17:02:13 -0600

    Add player setup method

--- lib/game.rb
+++ lib/game.rb
@@ -16,7 +16,6 @@ class Game
 
       until random_coordinates.count == cruiser.length
         random_coordinates << @computer_board.cells.keys.sample
-        require "pry"; binding.pry
       end
 
       if @computer_board.valid_placement?(cruiser, random_coordinates)
@@ -38,6 +37,49 @@ class Game
         break
       end
     end
+  end
+
+  def setup_player_board
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    #place cruiser
+    puts setup_player_board_message(cruiser)
+
+    loop do
+      player_coordinates = gets.chomp.upcase.split(" ")
+
+
+      if @player_board.valid_placement?(cruiser, player_coordinates)
+        @player_board.place(cruiser, player_coordinates)
+        break
+      end
+
+      puts "Those are invalid coordinates. Please try again:"
+    end
+
+    #place submarine
+    puts setup_player_board_message(submarine)
+
+    loop do
+      player_coordinates = gets.chomp.upcase.split(" ")
+
+
+      if @player_board.valid_placement?(submarine, player_coordinates)
+        @player_board.place(submarine, player_coordinates)
+        break
+      end
+
+      puts "Those are invalid coordinates. Please try again:"
+    end
+  end
+
+  def setup_player_board_message(ship)
+    "I have laid out my ships on the grid.
+    You now need to lay out your two ships.
+    The Cruiser is three units long and the Submarine is two units long.
+    #{player_board.render(true)}
+    Enter the squares for the #{ship.name} (#{ship.length} spaces):"
   end
 
 end
